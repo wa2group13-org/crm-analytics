f68af505b803d1ca5d9e126d076e9930aa6b60fe
Brendon Mendicino <brendonmendicino@yahoo.it>
2025-02-16 15:42:42 +0100

    fix: filter chain

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 val springCloudVersion by extra("2023.0.2")
 
 group = "it.polito.wa2.g13"
-version = "1.1.5"
+version = "1.1.6"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
--- src/main/kotlin/it/polito/wa2/g13/crmanalytics/configurations/SecurityConfig.kt
+++ src/main/kotlin/it/polito/wa2/g13/crmanalytics/configurations/SecurityConfig.kt
@@ -3,14 +3,20 @@ package it.polito.wa2.g13.crmanalytics.configurations
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.convert.converter.Converter
 import org.springframework.core.env.Environment
 import org.springframework.core.env.Profiles
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter
 import org.springframework.security.web.server.SecurityWebFilterChain
+import reactor.core.publisher.Mono
 
 @Configuration
 @EnableReactiveMethodSecurity
@@ -31,7 +37,11 @@ class SecurityConfig(
                 it.pathMatchers("/**").hasRole("ADMIN")
                 it.anyExchange().denyAll()
             }
-            .oauth2ResourceServer { it.jwt {} }
+            .oauth2ResourceServer { it ->
+                it.jwt { http ->
+                    http.jwtAuthenticationConverter(grantedAuthoritiesExtractor())
+                }
+            }
             .csrf { it.disable() }
             .cors { it.disable() }
             .build()
@@ -40,15 +50,23 @@ class SecurityConfig(
     /**
      * Get roles from the jwt
      */
-    @Bean
-    fun jwtAuthenticationConverterForKeycloak(): JwtAuthenticationConverter =
-        JwtAuthenticationConverter().apply {
-            setJwtGrantedAuthoritiesConverter { jwt ->
-                val realmAccess = jwt.getClaim<Map<String, Collection<String>>>("realm_access")
 
-                realmAccess["roles"]?.map { SimpleGrantedAuthority("ROLE_$it") }
-            }
+    fun grantedAuthoritiesExtractor(): Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+        val jwtAuthenticationConverter = JwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(GrantedAuthoritiesExtractor())
+        return ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter)
+    }
+
+    internal class GrantedAuthoritiesExtractor : Converter<Jwt, Collection<GrantedAuthority>> {
+        override fun convert(jwt: Jwt): Collection<GrantedAuthority> {
+            val realmAccess = jwt.getClaim<Map<String, Collection<String>>>("realm_access")
+
+            return realmAccess
+                ?.getOrDefault("roles", listOf())
+                ?.map { SimpleGrantedAuthority("ROLE_$it") }
+                ?: listOf()
         }
+    }
 }
 
 @Configuration
